8bcc5a77f6b08c22eeaff995cfd938a2b646f65a
stephania zambrano guerra <desa.web2@realtix.com.co>
2025-03-12 08:20:07 -0500

    estilos del sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,10 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [NavbarComponent, SidebarComponent, FooterComponent, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: 'app-root',
+    imports: [NavbarComponent, SidebarComponent, FooterComponent, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'banistmo_5.0';
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [ FooterComponent],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.less'
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.less'
 })
 export class SidebarComponent {
 
+    activeIndex: number | null = null;
+
+    public imagesDefault = [ 
+        { image: 'layer', type: 'dark', id: 1 },
+        { image: 'search', type: 'dark', id: 2 },
+        { image: 'add_layer', type: 'dark', id: 3 },
+        { image: 'print', type: 'dark', id: 4 },
+        { image: 'download', type: 'dark', id: 5 },
+        { image: 'statistics', type: 'dark', id: 6 },
+    ];
+
+    // public imagesOpenClick = [ 
+    //     { image: 'layer', type: 'ligth', id: 1 },
+    //     { image: 'search', type: 'ligth', id: 2 },
+    //     { image: 'add_layer', type: 'ligth', id: 3 },
+    //     { image: 'print', type: 'ligth', id: 4 },
+    //     { image: 'download', type: 'ligth', id: 5 },
+    //     { image: 'statistics', type: 'ligth', id: 6 },
+    // ];
+
+    onChangeImage(id: number, type: string): void {
+        this.activeIndex = this.activeIndex === id ? null : id;
+        
+        type == 'dark' ? this.imagesDefault[id-1].type = 'ligth' : this.imagesDefault[id-1].type = 'dark' ;
+    }
+
+    // isSidebarOpen = false;
+
+    // activeIndex: number | null = null;
+
+    // toggleSidebar(index: number) {
+    //     this.activeIndex = this.activeIndex === index ? null : index;
+    // }
 }
