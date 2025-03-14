06e345bbdc13479b47887b0abbef9103312a5e51
ingrevelo <ing.revelo@gmail.com>
2025-03-14 13:19:37 -0500

    geoserver service

--- src/app/core/services/geo-server.service.ts
+++ src/app/core/services/geo-server.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GeoServerService {
+  getLayer(layer: string) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
