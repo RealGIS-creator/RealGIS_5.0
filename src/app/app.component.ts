1da02c8cc4f3c8747d6bdf379b423aca92d2821c
Stephania Zambrano Guerra <desa.web2@realtix.com.co>
2025-04-01 11:03:00 -0500

    home en page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/home/navbar/navbar.component';
-import { SidebarComponent } from './components/home/sidebar/sidebar.component';
-import { FooterComponent } from './components/home/footer/footer.component';
-import { MapMainComponent } from './components/home/map-main/map-main.component';
 
 @Component({
     selector: 'app-root',
-    imports: [NavbarComponent, SidebarComponent, FooterComponent, RouterOutlet, MapMainComponent],
+    imports: [],
     templateUrl: './app.component.html',
     styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'banistmo_5.0';
 }
