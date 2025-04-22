2fe36c6a4f493d385e014747a187a01810874c0c
Stephania Zambrano Guerra <desa.web2@realtix.com.co>
2025-04-22 08:20:01 -0500

    iniciando leaflet desde inicio

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
-import * as L from 'leaflet';
-import 'leaflet.markercluster/dist/leaflet.markercluster.js';
+import L from 'leaflet';
+import 'leaflet.markercluster';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
