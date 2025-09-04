18a7afca69eb81a3466b1d55bec6f365439f5214
desarrolladoraweb2 <desa.web2@realtix.com.co>
2025-09-04 17:07:46 -0500

    refactor: update environment configuration for geoserver and backend URLs

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    // geoserverURL:  'http://10.0.12.9:8080/geoserver/', //geoserver genexus
-    geoserverURL: 'https://banistmoqa.realidad5.com/gisgeoserver/', // geoserver QA
-    // backendGN: 'http://10.0.12.9:8080/BanistmoGis_DesEnvironment/rest', // backend Genexus
-    backendGN: 'https://banistmoqa.realidad5.com/BanistmoGis_QA/rest', // backend QA
+    geoserverURL:  'http://10.0.12.9:8080/geoserver/', //geoserver genexus
+    // geoserverURL: 'https://banistmoqa.realidad5.com/gisgeoserver/', // geoserver QA
+    backendGN: 'http://10.0.12.9:8080/BanistmoGis_DesEnvironment/rest', // backend Genexus
+    // backendGN: 'https://banistmoqa.realidad5.com/BanistmoGis_QA/rest', // backend QA
   };
