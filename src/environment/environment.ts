d732201bdac37b21d67cdf399e0937e2a542c7a5
desarrolladoraweb2 <desa.web2@realtix.com.co>
2026-01-20 12:21:49 -0500

    refactor: update iframe comments for dashboard rendering and adjust environment configuration for production

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,19 @@
+// export const environment = {
+//     production: false,
+//     // geoserverURL:  'http://10.0.12.9:8080/geoserver/', //geoserver genexus
+//     // backendGN: 'http://10.0.12.9:8080/BanistmoGis_DesEnvironment/rest', // backend Genexus
+//   };
+
+// export const environment = {
+//   production: false,
+//   // geoserverURL: 'https://banistmo.realidad5.com/geoserver/', // geoserver QA
+//   // backendGN: 'https://banistmoqa.realidad5.com/BanistmoGis_QA/rest', // backend QA
+//   geoserverURL: 'https://geoqa.appst.ambientesbc.lab/geoserver/', // geoserver QA banco
+//   backendGN: 'https://geoqa.appst.ambientesbc.lab/BanistmoGis_QA/rest', // backend QA banco
+// };
+
 export const environment = {
-    production: true,
-    geoserverURL:  'http://10.0.12.9:8080/geoserver/', //geoserver genexus
-    // geoserverURL: 'https://banistmoqa.realidad5.com/gisgeoserver/', // geoserver QA
-    backendGN: 'http://10.0.12.9:8080/BanistmoGis_DesEnvironment/rest', // backend Genexus
-    // backendGN: 'https://banistmoqa.realidad5.com/BanistmoGis_QA/rest', // backend QA
-  };
+  production: true,
+  geoserverURL: 'https://geo.appst.bancolombia.corp/gisgeoserver/', // geoserver produccion banco
+  backendGN: 'https://geo.appst.bancolombia.corp/BanistmoGis_PRO/rest', // backend produccion banco
+};
